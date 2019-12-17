98ea148c2097d225d0c8bbbc3824f81958df8a74
christineRO <christinerole@icloud.com>
2019-12-17 02:02:57 +0000

    I almost finished :)

--- js/script.js
+++ js/script.js
@@ -10,9 +10,49 @@ function performLogic(buttonId, tileId) {
     if (player === "O") {
         player = "X";
     } else {
-        
+        player = "O"
+    }
+
+    totalNumberOfTurns =  totalNumberOfTurns + 1;
+
+    if (totalNumberOfTurns === 9) {
+        gameOver = true;
+    }
+    checkVerticalWins()
+    
+};
+
+function win(tileOne, tileTwo, tileThree){
+    let tile1 = $('#tile1').text();
+    let tile2 = $('#tile2').text();
+    let tile3 = $('#tile3').text();
+    let tile4 = $('#tile4').text();
+    let tile5 = $('#tile5').text();
+    let tile6 = $('#tile6').text();
+    let tile7 = $('#tile7').text();
+    let tile8 = $('#tile8').text();
+    let tile9 = $('#tile9').text();
+    if ((tileOne && tileTwo && tileThree) === player){
+        return player
     }
 };
+
+function checkVerticalWins(){
+    let tile1 = $('#tile1').text();
+    let tile2 = $('#tile2').text();
+    let tile3 = $('#tile3').text();
+    let tile4 = $('#tile4').text();
+    let tile5 = $('#tile5').text();
+    let tile6 = $('#tile6').text();
+    let tile7 = $('#tile7').text();
+    let tile8 = $('#tile8').text();
+    let tile9 = $('#tile9').text();    
+    
+    if (tile1 === tile2 && tile2 === tile3){
+        console.log("WOOHOO")
+    }
+};
+
 
 $("#button1").click(function() {
     performLogic("#button1","#tile1");
